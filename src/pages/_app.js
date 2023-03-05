fa36228d5ed860a22fbc8d019343e46c3a3e7b6b
tanuj <tanujbhatt644@gmail.com>
2023-03-05 07:42:15 +0530

    redeploying...!

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import Theme from '../styles/theme';
 import 'react-quill/dist/quill.snow.css'
-import '../styles/globals.css'
+import '../styles/globals'
 
 export default function App({ Component, pageProps }) {
   return (
