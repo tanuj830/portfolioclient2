1389a71d2aa8a7501a82b2da391f916a623158a5
tanuj <tanujbhatt644@gmail.com>
2023-03-05 08:49:43 +0530

    preloader added to project by id page...!

--- src/pages/blog/[exploreblog].js
+++ src/pages/blog/[exploreblog].js
@@ -21,14 +21,25 @@ axios.get(`https://portfolio-38ir.onrender.com/blog/${id}`).then(res=>setBlog(re
   return (
     <>
 <div className='container'><Header/></div>
-    <Section>
-      <SectionTitle>
-        {blog.title} 
-      </SectionTitle>
-      <small style={{color:"grey", marginBottom:30}}>Published On: {blog.Date ? blog.Date.slice(0,10) : blog.Date}</small>
-      <SectionText dangerouslySetInnerHTML={{ __html : blog.disp}}>
-      </SectionText>
-    </Section>
+ {
+  !blog.disp ?
+  <Section>
+      <div className=" w-100 flex flex-column">
+<div className="bg-slate-500 animate-pulse h-10 w-50 rounded-full"></div>
+<div className="bg-slate-500 animate-pulse h-6 w-40 rounded-full mt-4"></div>
+<div className="bg-slate-500 animate-pulse h-80 w-90 rounded-xl mt-16"></div>
+</div>
+  </Section>
+  :
+  <Section>
+  <SectionTitle>
+    {blog.title} 
+  </SectionTitle>
+  <small style={{color:"grey", marginBottom:30}}>Published On: {blog.Date ? blog.Date.slice(0,10) : blog.Date}</small>
+  <SectionText dangerouslySetInnerHTML={{ __html : blog.disp}}>
+  </SectionText>
+</Section>
+ }
     <Footer/>
   </>
   )
