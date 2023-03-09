bb4892922b081271aa453bfc61047d0c29de802f
tanuj <tanujbhatt644@gmail.com>
2023-03-09 17:27:09 +0530

    Project Component Updated...!

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,9 +32,7 @@ export default class MyDocument extends Document {
       <Html lang='en-GB'>
         <Head>
           <title>tanujbhatt.in</title>
-          
-          <link href="https://tse4.mm.bing.net/th?id=OIP.2q0z-nkTbwR-cED5f-_iOAHaHa&pid=Api&P=0" rel='icon'/>
-  <meta name="description" content="coding"></meta>
+            <meta name="description" content="xxxyyyzzz"></meta>
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
           <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
