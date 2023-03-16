0bf3f59b4af20b157587598e1a25196059daea40
tanuj <tanujbhatt644@gmail.com>
2023-03-16 20:00:00 +0530

    hero section updated ...!

---
+++ src/pages/services/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
+import { Section } from '../../styles/GlobalComponents'
+
+const index = () => {
+  return (
+    <>
+    <Header />
+    
+    <Section>
+        Services Page
+    </Section>
+
+    <Footer />   
+    </>
+  )
+}
+
+export default index
