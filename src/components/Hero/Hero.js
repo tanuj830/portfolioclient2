a00e0e2218d049018d2e10d78a1fed72879fa517
tanuj <tanujbhatt644@gmail.com>
2023-03-16 20:02:15 +0530

    hero section updated ...!

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
         My Personal Portfolio */}
       </SectionTitle>
       <SectionText>
-      Tanuj Bhatt is a skilled web developer and blockchain enthusiast with a passion for innovative technology. With extensive experience in designing and developing websites and blockchain applications. His expertise in web development includes a wide range of programming languages, such as HTML, CSS, JavaScript(ReactJS, NextJS, NodeJS, Web3JS, NodeJS, ExpressJS) & MongoDB etc. Additionally, his understanding of blockchain technology has enabled him to create decentralized applications that are secure, transparent, and efficient. My commitment to staying up-to-date with the latest advancements in the industry has made him a valuable asset to any team he works with. With a strong work ethic and a dedication to delivering high-quality results, Tanuj is eager to take on new challenges and contribute to the success of his clients and colleagues.
+      Tanuj Bhatt is a skilled web developer and blockchain enthusiast with a passion for innovative technology. With extensive experience in designing and developing websites and blockchain applications. My commitment to staying up-to-date with the latest advancements in the industry has made him a valuable asset to any team he works with. With a strong work ethic and a dedication to delivering high-quality results, Tanuj is eager to take on new challenges and contribute to the success of his clients and colleagues.
       </SectionText>
       <Button onClick={()=>window.location = "/blog"}>Read Blogs</Button>
     </LeftSection>
